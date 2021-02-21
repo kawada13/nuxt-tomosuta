ffe6522d95bc9d74d98445c4caedee477c84f84f
kawada <vgnsl94@gmail.com>
2021-02-22 00:08:46 +0900

    vuex追加

---
+++ pages/todos.vue
@@ -0,0 +1,21 @@
+<template>
+  <div style="margin: 20px">
+    <input type="text" placeholder="タスクの追加" @keyup.enter="addTodo">
+  </div>
+</template>
+
+<script>
+import { mapMutations } from 'vuex'
+export default {
+  methods: {
+    addTodo(e) {
+      this.$store.commit('todos/add', e.target.value)
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
