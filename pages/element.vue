a9837fcdc94419facafcbc8b00b4182354e01afb
kawada <vgnsl94@gmail.com>
2021-02-21 19:31:03 +0900

    element追加

---
+++ pages/element.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>elementページ</h1>
+    <el-button @click="visible = true">Button</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
