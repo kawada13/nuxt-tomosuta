a8086cb24e80f25ff919ab0be195fa4d44b78d01
kawada <vgnsl94@gmail.com>
2021-02-21 19:41:07 +0900

    element

--- pages/element.vue
+++ pages/element.vue
@@ -1,12 +1,20 @@
 <template>
-  <div>
+  <div style="padding: 30px" >
     <h1>elementページ</h1>
-    <el-button @click="visible = true">Button</el-button>
+    <el-button @click="visible = true">ダイアログを開く</el-button>
+    <el-dialog :visible.sync="visible" title="確認">
+      <p>処理されました</p>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      visible: false
+    }
+  },
 
 }
 </script>
