0d41a8dc9b6b33aba35425f040fb2a6d1d5df45b
kawada <vgnsl94@gmail.com>
2021-02-21 19:06:18 +0900

    ブログ詳細ページ作成

---
+++ pages/blog/_id.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    ブログ詳細だお
+    <p>これは{{$route.params.id}}エントリーです</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/blog/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    ブログ一覧だお
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
